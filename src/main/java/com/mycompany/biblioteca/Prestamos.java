d280fb5ef4d74080e634fed2e53e17de75ac7ba8
hector <hjguevara@unicesar.edu.co>
2026-08-17 17:28:10 -0500

    feat crear clase prestamo que relaciona cliente y libro

---
+++ src/main/java/com/mycompany/biblioteca/Prestamos.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.biblioteca;
+
+/**
+ *
+ * @author Keiner
+ */
+public class Prestamos {
+    String idPrestamo;
+    Cliente cliente;
+    Libro libro;
+    String fecha;
+    String estado;
+
+    public String getIdPrestamo() {
+        return idPrestamo;
+    }
+
+    public void setIdPrestamo(String idPrestamo) {
+        this.idPrestamo = idPrestamo;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Libro getLibro() {
+        return libro;
+    }
+
+    public void setLibro(Libro libro) {
+        this.libro = libro;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+}
--- src/main/java/com/mycompany/biblioteca/main.java
+++ src/main/java/com/mycompany/biblioteca/main.java
@@ -4,13 +4,9 @@
 
 package com.mycompany.biblioteca;
 
-/**
- *
- * @author Keiner
- */
 public class main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+      
     }
 }
