9302b4ba421c7ee5edf49bc9aebe3a1fcb4e853d
hector <hjguevara@unicesar.edu.co>
2026-08-17 17:08:33 -0500

    feat : implementar UDPATE de libro

--- src/main/java/com/mycompany/biblioteca/main.java
+++ src/main/java/com/mycompany/biblioteca/main.java
@@ -40,19 +40,50 @@ public class main {
             System.out.println("----------------------");
         }
     }
+
     public static Libro buscar() {
         System.out.println("consulta selectiva de libro");
         System.out.println("codigo del libro");
         String codigo = sc.nextLine();
         for (Libro libro : libros) {
-            if(libro.getCodigo().equals(codigo)){
-            System.out.println("codigo" + libro.getCodigo());
-            System.out.println("titulo" + libro.getTitulo());
-            System.out.println("año de publicacion" + libro.getYearpublic());
-            System.out.println("autor" + libro.getAutor());
-            System.out.println("estado" + libro.isDisponible());
-            System.out.println("----------------------");
+            if (libro.getCodigo().equals(codigo)) {
+                System.out.println("codigo" + libro.getCodigo());
+                System.out.println("titulo" + libro.getTitulo());
+                System.out.println("año de publicacion" + libro.getYearpublic());
+                System.out.println("autor" + libro.getAutor());
+                System.out.println("estado" + libro.isDisponible());
+                return libro;
             }
         }
+        System.out.println("Cliente no encontrado");
+        return null;
+    }
+
+    public static Libro actualizar() {
+        System.out.println("consulta selectiva de libro");
+        System.out.println("codigo del libro");
+        String codigo = sc.nextLine();
+        for (Libro libro : libros) {
+            if (libro.getCodigo().equals(codigo)) {
+                System.out.println("codigo" + libro.getCodigo());
+                System.out.println("titulo" + libro.getTitulo());
+                System.out.println("año de publicacion" + libro.getYearpublic());
+                System.out.println("autor" + libro.getAutor());
+                System.out.println("estado" + libro.isDisponible());
+                System.out.println("----------------------");
+                System.out.println("nuevo titulo");
+                String titulo=sc.nextLine();
+                System.out.println("nuevo año de publicacion");
+                String yearpublic=sc.nextLine();
+                System.out.println("nuevo autor");
+                String autor=sc.nextLine();
+                libro.setTitulo(titulo);
+                libro.setYearpublic(yearpublic);
+                libro.setAutor(autor);
+                return libro;
+            }
+        }
+        System.out.println("Cliente no encontrado");
+        return null;
     }
 }
